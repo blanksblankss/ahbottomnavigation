08cb3986bf9c2a8b9e5ce575e271da1dbef744c1
Aurelien Hubert <aurel.hubert@gmail.com>
2016-07-08 10:40:31 +0200

    Added a setup method with colors for tab items

--- ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigationAdapter.java
+++ ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigationAdapter.java
@@ -1,6 +1,7 @@
 package com.aurelhubert.ahbottomnavigation;
 
 import android.app.Activity;
+import android.support.annotation.ColorInt;
 import android.support.annotation.MenuRes;
 import android.support.v7.widget.PopupMenu;
 import android.view.Menu;
@@ -30,11 +31,21 @@ public class AHBottomNavigationAdapter {
 	}
 
 	/**
-	 * Setup with bottom navigation
+	 * Setup bottom navigation
 	 *
-	 * @param ahBottomNavigation
+	 * @param ahBottomNavigation AHBottomNavigation: Bottom navigation
 	 */
 	public void setupWithBottomNavigation(AHBottomNavigation ahBottomNavigation) {
+		setupWithBottomNavigation(ahBottomNavigation, null);
+	}
+
+	/**
+	 * Setup bottom navigation (with colors)
+	 *
+	 * @param ahBottomNavigation AHBottomNavigation: Bottom navigation
+	 * @param colors             int[]: Colors of the item
+	 */
+	public void setupWithBottomNavigation(AHBottomNavigation ahBottomNavigation, @ColorInt int[] colors) {
 		if (navigationItems == null) {
 			navigationItems = new ArrayList<>();
 		} else {
@@ -44,8 +55,13 @@ public class AHBottomNavigationAdapter {
 		if (mMenu != null) {
 			for (int i = 0; i < mMenu.size(); i++) {
 				MenuItem item = mMenu.getItem(i);
-				AHBottomNavigationItem navigationItem = new AHBottomNavigationItem(String.valueOf(item.getTitle()), item.getIcon());
-				navigationItems.add(navigationItem);
+				if (colors != null && colors.length >= mMenu.size() && colors[i] != 0) {
+					AHBottomNavigationItem navigationItem = new AHBottomNavigationItem(String.valueOf(item.getTitle()), item.getIcon(), colors[i]);
+					navigationItems.add(navigationItem);
+				} else {
+					AHBottomNavigationItem navigationItem = new AHBottomNavigationItem(String.valueOf(item.getTitle()), item.getIcon());
+					navigationItems.add(navigationItem);
+				}
 			}
 			ahBottomNavigation.removeAllItems();
 			ahBottomNavigation.addItems(navigationItems);
