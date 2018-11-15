f7a1a502adc86da8f784c1fc033004066f22cd45
Alexandre Pereira Nunes <alexandre.nunes@gmail.com>
2018-11-15 13:45:18 -0200

    Migrate project to AndroidX

--- demo/src/main/java/com/aurelhubert/ahbottomnavigation/demo/DemoViewPagerAdapter.java
+++ demo/src/main/java/com/aurelhubert/ahbottomnavigation/demo/DemoViewPagerAdapter.java
@@ -1,7 +1,7 @@
 package com.aurelhubert.ahbottomnavigation.demo;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
